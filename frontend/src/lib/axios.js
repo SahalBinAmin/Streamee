44119577947e7a4225115c865be06117b171f3c7
Sahal Bin Amnin <sahalbinamin.37@gmail.com>
2025-09-07 10:37:41 +0500

    frontEnd deployment ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
 });
