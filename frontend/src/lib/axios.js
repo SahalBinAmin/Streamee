96c26cbe37438d52a6de81683aa7bcba0c75b3ed
Sahal Bin Amnin <sahalbinamin.37@gmail.com>
2025-09-07 10:55:41 +0500

    Reference Error debugged

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
